51d6abc15cd03ea41e59331eca171abf9a191bde
Kritz23 <kritz2399@gmail.com>
2023-09-26 20:39:58 +1000

    update country

--- scripts/src/author_info.py
+++ scripts/src/author_info.py
@@ -1,4 +1,5 @@
 import json
+import pycountry
 import pandas as pd
 
 # Define a function to extract and format the author name
@@ -7,13 +8,17 @@ def extract_author_info(row):
     
     if isinstance(authors, list) and len(authors) > 0:
         last_author = authors[-1]
-        first_name = last_author.get('first_name', 'NA')
-        last_name = last_author.get('last_name', 'NA')
+        first_name = last_author.get('first_name', '')
+        last_name = last_author.get('last_name', '')
         affiliations = last_author.get('affiliations', [])
         
         if affiliations:
-            institution = affiliations[0].get('name', 'NA')
-            country_code = affiliations[0].get('country_code', 'NA')
-            return f"{first_name} {last_name}", institution, country_code
+            institution = affiliations[0].get('name', '')
+            country_code = affiliations[0].get('country_code', '')
+            try:
+                country_name = pycountry.countries.get(alpha_2=country_code).name
+            except AttributeError:
+                country_name = ''
+            return f"{first_name} {last_name}", institution, country_name
         
     return '', '', ''
